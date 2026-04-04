b567ebedd46d5bbf8e94b1a31a567b223e51bba3
danprog24 <tobason17@gmail.com>
2026-04-04 22:57:52 +0100

    fix: hardcode backend URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://connectly-chat-production.up.railway.app",
   withCredentials: true,
 });
 
--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -5,6 +5,7 @@ export const useRegister = () => {
   return useMutation({
     mutationFn: async (data: { username: string; password: string }) => {
       const response = await axios.post("/auth/register", data);
+      console.log("API URL:", import.meta.env.VITE_API_URL)
       return response.data;
     },
   });
